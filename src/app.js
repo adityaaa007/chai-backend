e13293941c50c0cc4ce5e92e410821418fc15c62
ADITYA <aditya.singh825825@gmail.com>
2024-05-13 21:04:41 +0530

    added dashboard and healthcheck logic and project completed :)

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ import tweetRouter from "./routes/tweet.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
+import healthCheckRouter from "./routes/healthcheck.routes.js";
 
 const app = express();
 
@@ -31,5 +33,7 @@ app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/healthcheck", healthCheckRouter)
 
 export default app;
---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,80 @@
+import mongoose from "mongoose";
+import { Video } from "../models/video.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const getChannelStats = asyncHandler(async (req, res) => {
+  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+
+  const data = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        foreignField: "channel",
+        localField: "owner",
+        as: "totalSubscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        foreignField: "video",
+        localField: "_id",
+        as: "totalLikes",
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalViews: { $sum: "$views" },
+        totalVideos: { $sum: 1 },
+        totalSubscribers: { $sum: { $size: "$totalSubscribers" } },
+        totalLikes: { $sum: { $size: "$totalLikes" } },
+      },
+    },
+  ]);
+
+  if (!data) {
+    throw new ApiError(
+      500,
+      "Something went wrong while fetching dashboard data"
+    );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, data, "Dashboard data fetched successfully"));
+});
+
+const getChannelVideos = asyncHandler(async (req, res) => {
+  // TODO: Get all the videos uploaded by the channel
+
+  const videos = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(req.user._id)
+      }
+    }
+  ])
+
+  if (!videos) {
+    throw new ApiError(
+      500,
+      "Something went wrong while fetching channel videos"
+    );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, videos, "Channel videos fetched successfully"));
+});
+
+export { getChannelStats, getChannelVideos };
